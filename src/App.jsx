865ae7122b74a2b1e8748533e0462e8e876b3baf
neondoll <bonni_1998@mail.ru>
2024-09-21 21:10:59 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ function App() {
   return (
     <>
       <List items={users} onItemClick={handleItemClick} />
-      <Details item={activeUser} />
+      <Details info={activeUser} />
     </>
   );
 }
